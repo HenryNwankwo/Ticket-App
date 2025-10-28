6b734caee1cfc220fa96c08bcf15ef7617df082b
Nwankwo Henry Ifunanya <nameisifunanya@gmail.com>
2025-10-28 13:49:35 +0100

    Added the needed configs and setups

---
+++ ticketapp-vue/tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        statusOpen: '#138000',
+        statusProgress: '#c75a00',
+        statusClosed: '#4b5563'
+      }
+    }
+  },
+  plugins: [],
+}
